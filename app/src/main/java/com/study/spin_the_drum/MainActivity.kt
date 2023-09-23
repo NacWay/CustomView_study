ee07254e50b43235f6d17a64f267b752a4567141
Maksim Malov <maksim-masia@mail.ru>
2023-09-23 15:12:09 +0500

    finish HW(without slider/mb post it later)

--- app/src/main/java/com/study/spin_the_drum/MainActivity.kt
+++ app/src/main/java/com/study/spin_the_drum/MainActivity.kt
@@ -1,20 +1,91 @@
 package com.study.spin_the_drum
 
-
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.ViewCompat.animate
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         val image: ImageView = findViewById(R.id.imageView)
+         val text: TextView = findViewById(R.id.sectorText)
+         val resetBtn: Button = findViewById(R.id.reset)
+         val customView: MyCustomView = findViewById(R.id.customView)
 
-        val customView: MyCustomView = findViewById(R.id.customView)
-        val animanimate = customView.animate()
-            .rotation((100 + Math.random() * 4000).toFloat())
-            .setDuration(1200)
-            animanimate.start()
+        //Крутите барабан!
+        customView.setOnClickListener {
+
+            image.setImageResource(Color.TRANSPARENT)  //обнуляем при новом нажатии
+            text.text=""                               //обнуляем при новом нажатии
+
+            val rotationCount: Float = (500 + Math.random() * 3000).toFloat()  //рандомное число кручения барабана
+            var sectorPosition: Int = 0                                        //позиция после кручения
+
+            //Крутим барабан
+            animate(customView)
+                .rotation(-rotationCount)
+                .setDuration(4000)
+                .start()
+
+            Handler(Looper.getMainLooper()).postDelayed(        //немного замедляем поток чтобы показать результат кручения барабана после анимации
+                {
+                    //расчет позиции
+                    sectorPosition =
+                ((rotationCount.toDouble() / 360).toString().substringAfterLast('.')
+                    .substring(0, 4).toDouble() / 10000 * 360).toInt()
+                //устанавливаем текст
+                text.text = findSelectedSector(sectorPosition, image)
+                },4010
+            )
+        }
+
+        //Кнопка reset
+        resetBtn.setOnClickListener {
+            image.setImageResource(Color.TRANSPARENT)
+            text.text=""
+        }
+
 
     }
+
+    //Подарки в студию!
+    private fun findSelectedSector(sectorPositin: Int, view: ImageView): String{
+        when {
+            sectorPositin in 0..51 -> return "Приз: Стиральная машина"
+            sectorPositin in 52..102 -> return downloadImage("https://gratisography.com/wp-content/uploads/2023/09/gratisography-duck-doctor-free-stock-photo-1170x780.jpg", view)
+            sectorPositin in 103..153 -> return "Приз: Шапка из оленя"
+            sectorPositin in 154..204 -> return downloadImage("https://gratisography.com/wp-content/uploads/2023/09/gratisography-parrot-pirate-free-stock-photo-1170x780.jpg", view)
+            sectorPositin in 205..255 -> return "Приз: Ведро кортошки"
+            sectorPositin in 256..306 -> return downloadImage("https://gratisography.com/wp-content/uploads/2023/06/gratisography-flying-squirrel-free-stock-photo-1170x780.jpg", view)
+            sectorPositin in 307..360 -> return "Главный приз: Автомобиль"
+        }
+        return "Барабан улетел"
+    }
+
+    //грузим изображение
+    private fun downloadImage(URL: String, view: ImageView): String{
+        Picasso.get()
+            .load(URL)
+            .placeholder(android.R.drawable.stat_notify_sync)
+            .error(androidx.constraintlayout.widget.R.drawable.abc_btn_switch_to_on_mtrl_00012)
+            .resize(300,200)
+            .centerCrop()
+            .into(view)
+        return ""
+    }
+
+
 }
--- app/src/main/java/com/study/spin_the_drum/MyCustomView.kt
+++ app/src/main/java/com/study/spin_the_drum/MyCustomView.kt
@@ -2,17 +2,19 @@ package com.study.spin_the_drum
 
 import android.annotation.SuppressLint
 import android.content.Context
+
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Path
 import android.graphics.RectF
+import android.preference.PreferenceManager
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
 
-@Suppress("UNREACHABLE_CODE")
+
+
 class MyCustomView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
     private val paintRed = Paint()
@@ -22,6 +24,7 @@ class MyCustomView(context: Context, attrs: AttributeSet?) : View(context, attrs
     private val paintLightblue = Paint()
     private val paintBlue = Paint()
     private val paintPurple = Paint()
+
 
     @SuppressLint("ResourceAsColor", "DrawAllocation")
     override fun onDraw(canvas: Canvas) {
@@ -50,43 +53,57 @@ class MyCustomView(context: Context, attrs: AttributeSet?) : View(context, attrs
         paintPurple.color = ContextCompat.getColor(context, R.color.purple)
         paintPurple.style = Paint.Style.FILL
 
-
-
         // Рисуем Сектора
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 0f, 51f, true, paintRed)
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 51f, 51f, true, paintOrange)
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 102f, 51f, true, paintYellow)
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 153f, 51f, true, paintGreen)
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 204f, 51f, true, paintLightblue)
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 255f, 51f, true, paintBlue)
-        canvas.drawArc(RectF(10f, 10f, (width-10).toFloat(), (height-10).toFloat()), 306f, 54f, true, paintPurple)
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            0f,
+            51f,
+            true,
+            paintRed
+        )
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            51f,
+            51f,
+            true,
+            paintOrange
+        )
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            102f,
+            51f,
+            true,
+            paintYellow
+        )
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            153f,
+            51f,
+            true,
+            paintGreen
+        )
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            204f,
+            51f,
+            true,
+            paintLightblue
+        )
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            255f,
+            51f,
+            true,
+            paintBlue
+        )
+        canvas.drawArc(
+            RectF(10f, 10f, (width - 10).toFloat(), (height - 10).toFloat()),
+            306f,
+            54f,
+            true,
+            paintPurple
+        )
 
 
-    }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        when (event?.action) {
-
-            MotionEvent.ACTION_DOWN -> {
-                animate()
-                    .rotation((1500+Math.random()*3000).toFloat())
-                    .setDuration(4000)
-                    .start()
-
-
-                return false
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-                // обработка перемещения пальца по экрану
-                return true
-            }
-
-            MotionEvent.ACTION_UP -> {
-                // обработка отпускания пальца от экрана
-                return true
-            }
-        }
-        return super.onTouchEvent(event)
     }
 }
